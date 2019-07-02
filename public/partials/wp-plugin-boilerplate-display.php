a270656edc1fa2f9406736beb7551ab90114a098
Pierre Lannoy <pierre@lannoy.fr>
2019-07-02 22:12:10 +0200

    Public-facing view for the plugin.

---
+++ public/partials/wp-plugin-boilerplate-display.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Provide a public-facing view for the plugin
+ *
+ * This file is used to markup the public-facing aspects of the plugin.
+ *
+ * @since 1.0.0
+ *
+ * @package    Adr_Sync
+ * @subpackage Adr_Sync/public/partials
+ */
+
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
