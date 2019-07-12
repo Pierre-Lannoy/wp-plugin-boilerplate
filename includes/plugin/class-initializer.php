93c340a0a1fcb23765ddc9402a632267bf43cf41
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-07-12 18:21:06 +0200

    Adds mechanism for post 'init' hook.

---
+++ includes/plugin/class-initializer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin initialization handling.
+ *
+ * @package Plugin
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+
+namespace WPPluginBoilerplate\Plugin;
+
+/**
+ * Fired after 'init' hook.
+ *
+ * This class defines all code necessary to run during the plugin's initialization.
+ *
+ * @package Plugin
+ * @author  Pierre Lannoy <https://pierre.lannoy.fr/>.
+ * @since   1.0.0
+ */
+class Initializer {
+
+	/**
+	 * Initializes the plugin.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function initialize() {
+
+	}
+
+}
