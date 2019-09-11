0062c9c4310c660471d186b69e1a64c9b224da8d
Pierre Lannoy <pierre.lannoy@adeo.com>
2019-09-11 19:08:20 +0200

    Fix "Trying to get property 'blogname' of non-object" when the blog has been deleted.

--- includes/system/class-blog.php
+++ includes/system/class-blog.php
@@ -9,7 +9,7 @@
  * @since   1.0.0
  */
 
-namespace Decalog\System;
+namespace WPPluginBoilerplate\System;
 
 /**
  * Define the Blog (site) functionality.
@@ -74,14 +74,14 @@ class Blog {
 	/**
 	 * Get the current blog id.
 	 *
-	 * @param   mixed   $default    Optional. Default value to return if blog is not detected.
+	 * @param   mixed $default    Optional. Default value to return if blog is not detected.
 	 * @return  mixed|integer The blog id if detected, null otherwise.
 	 * @since   1.0.0
 	 */
-	public static function get_current_blog_id($default = null) {
+	public static function get_current_blog_id( $default = null ) {
 		$blog_id = $default;
-		$id = get_current_blog_id();
-		if ( $id && is_numeric($id) && $id >0) {
+		$id      = get_current_blog_id();
+		if ( $id && is_numeric( $id ) && $id > 0 ) {
 			$blog_id = $id;
 		}
 		return $blog_id;
@@ -90,11 +90,11 @@ class Blog {
 	/**
 	 * Get the current blog name.
 	 *
-	 * @param   string  $default    Optional. Default value to return if blog is not detected.
+	 * @param   string $default    Optional. Default value to return if blog is not detected.
 	 * @return  string  The current blog name if detected, "anonymous" otherwise.
 	 * @since   1.0.0
 	 */
-	public static function get_current_blog_name($default = 'unknown' ) {
+	public static function get_current_blog_name( $default = 'unknown' ) {
 		return self::get_blog_name( self::get_current_blog_id(), $default );
 	}
 
