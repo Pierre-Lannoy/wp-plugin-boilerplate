0b6ad297a9b82c9b0f8e0bf722dea65a219bb7e1
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-05-02 13:01:59 +0200

    Fixes issues with IIS/Win10.

--- includes/system/class-environment.php
+++ includes/system/class-environment.php
@@ -35,6 +35,7 @@ class Environment {
 	 */
 	public static function init() {
 		$plugin_path         = str_replace( WPPB_SLUG . '/includes/system/', WPPB_SLUG . '/', plugin_dir_path( __FILE__ ) );
+		$plugin_path         = str_replace( WPPB_SLUG . '\includes\system/', WPPB_SLUG . '/', $plugin_path );
 		$plugin_url          = str_replace( WPPB_SLUG . '/includes/system/', WPPB_SLUG . '/', plugin_dir_url( __FILE__ ) );
 		$plugin_relative_url = str_replace( get_site_url() . '/', '', $plugin_url );
 		define( 'WPPB_PLUGIN_DIR', $plugin_path );
