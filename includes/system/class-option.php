79c07a0f282885432a35e897d9e1b0e05d61d599
Pierre Lannoy <pierre.lannoy@adeo.com>
2020-05-09 16:18:02 +0200

    Forces option to "0" when they are set at "false".

--- includes/system/class-option.php
+++ includes/system/class-option.php
@@ -119,6 +119,9 @@ class Option {
 	 * @since 1.0.0
 	 */
 	public static function site_set( $option, $value, $autoload = null ) {
+		if ( false === $value ) {
+			$value = 0;
+		}
 		return update_option( WPPB_PRODUCT_ABBREVIATION . '_' . $option, $value, $autoload );
 	}
 
@@ -131,6 +134,9 @@ class Option {
 	 * @since 1.0.0
 	 */
 	public static function network_set( $option, $value ) {
+		if ( false === $value ) {
+			$value = 0;
+		}
 		return update_site_option( WPPB_PRODUCT_ABBREVIATION . '_' . $option, $value );
 	}
 
